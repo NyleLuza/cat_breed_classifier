6479f6896711cf0afa127d76133587b014b84de0
NyleLuza <Jluza21@gmail.com>
2026-05-19 22:05:43 -0700

    init model

--- src/model.py
+++ src/model.py
@@ -1,1 +1,10 @@
-print("Hello")
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# defining network
+
+class Model(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.conv1 = nn.Conv2d(1,20,5)
+        self.conv2 - nn.Conv2d(20,20,5)
